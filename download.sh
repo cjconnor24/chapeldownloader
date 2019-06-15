dcec1742e404c7a8805938c70545ca6526980d17
Chris Connor <cjconnor24@connordesign.com>
2019-06-15 11:39:40 +0100

    adding a sanity check to see if it was successful

--- download.sh
+++ download.sh
@@ -12,8 +12,20 @@ FULLURL=$BASEURL$MEDIABASE$STARTNUMBER".ts"
 while [ true ]
 do
 
-    curl $BASEURL$MEDIABASE$STARTNUMBER".ts" -s -o $STARTNUMBER".ts" -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36' -H 'Referer: https://www.gla.ac.uk/myglasgow/chaplaincy/webcam/' -H 'Origin: https://www.gla.ac.uk' --compressed -w "%{url_effective}\t${RUNTIME}\t${i}\t%{http_code}\t%{time_pretransfer}\t%{time_starttransfer}\t%{time_total}\t$(echo $HTTP_METHOD)\t$(date)\n"
+# echo $STARTNUMBER
+
+    RESULT=$(curl $BASEURL$MEDIABASE$STARTNUMBER".ts" -s -o $STARTNUMBER".ts" -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/75.0.3770.80 Safari/537.36' -H 'Referer: https://www.gla.ac.uk/myglasgow/chaplaincy/webcam/' -H 'Origin: https://www.gla.ac.uk' --compressed -w "%{http_code}")
+
+if [ $RESULT -eq 200 ]
+then
+echo -e $STARTNUMBER"\t"$RESULT
+let "STARTNUMBER++"
+else
+echo -e $STARTNUMBER"\t"$RESULT
+fi
+
+    
     sleep 7s
-    let "STARTNUMBER++"
+    
     
 done
